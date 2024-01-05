a7e39003031b6c824dff7e8b899f6e02708d238e
Alexus91 <mrfadlkamal@gmail.com>
2024-01-05 00:16:45 +0000

    fixing requirement by putting comments on top

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+function def pascal_triangle(n): that returns a list of lists of integers
+representing the Pascal's triangle of n
+"""
+
 
 def print_triangle(triangle):
     """
