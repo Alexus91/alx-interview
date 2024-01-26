94a4fd2bbe9c9f53c298dbc3e0bc23381e7253a1
root <Alexus91>
2024-01-26 01:58:04 +0000

    0. Log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,53 +1,45 @@
 #!/usr/bin/python3
 """
-Log Parsing Script: Reads standard input line by line and computes metrics
+A script: Reads standard input line by line and computes metrics
 """
 
 
-def parse_logs():
+def parseLogs():
     """
-    Parses logs from standard input and generates reports
-    Reports:
-        * Prints log size after reading every 10 lines & at KeyboardInterrupt
-    Raises:
-        KeyboardInterrupt (Exception): handles this exception and raises it
-    """
+    Reads logs from standard input and generates reports """
     stdin = __import__('sys').stdin
-    line_number = 0
-    file_size = 0
-    status_codes = {'200': 0, '301': 0, '400': 0, '401': 0,
-                    '403': 0, '404': 0, '405': 0, '500': 0}
+    lineNumber = 0
+    fileSize = 0
+    statusCodes = {'200': 0, '301': 0, '400': 0, '401': 0,
+                   '403': 0, '404': 0, '405': 0, '500': 0}
     try:
         for line in stdin:
-            line_number += 1
+            lineNumber += 1
             line = line.split()
             try:
-                file_size += int(line[-1])
+                fileSize += int(line[-1])
                 status_code = line[-2]
-                if status_code in status_codes:
-                    status_codes[status_code] += 1
+                if status_code in statusCodes:
+                    statusCodes[status_code] += 1
             except (IndexError, ValueError):
                 pass
-            if line_number == 10:
-                report(file_size, status_codes)
-                line_number = 0
-        report(file_size, status_codes)
+            if lineNumber == 10:
+                report(fileSize, statusCodes)
+                lineNumber = 0
+        report(fileSize, statusCodes)
     except KeyboardInterrupt as e:
-        report(file_size, status_codes)
+        report(fileSize, statusCodes)
         raise
 
 
-def report(file_size, status_codes):
+def report(fileSize, statusCodes):
     """
     Prints generated report to standard output
-    Args:
-        file_size (int): total log size after every 10 successfully read line
-        status_codes (dict): dictionary of status codes and counts
     """
-    print("File size: {}".format(file_size))
-    for code in sorted(status_codes.keys()):
-        print("{}: {}".format(code, status_codes[code]))
+    print("File size: {}".format(fileSize))
+    for code in sorted(statusCodes.keys()):
+        print("{}: {}".format(code, statusCodes[code]))
 
 
 if __name__ == '__main__':
-    parse_logs()
+    parseLogs()
